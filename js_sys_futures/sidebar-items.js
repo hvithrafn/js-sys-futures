7a01f93e3b73c6bf91c3e04dbf7481bb9fda50fb
hvithrafn <hvithrafn@users.noreply.github.com>
2021-01-28 08:30:42 +0000

    deploy: 7aef0ba12017a08d45f3f23d5d3d4f85b8cf09a5

--- js_sys_futures/sidebar-items.js
+++ js_sys_futures/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["JsAsyncRead",""],["JsStream",""]]});
+initSidebarItems({"fn":[["future_to_promise","Converts a Rust `Future` into a JavaScript `Promise`."],["spawn_local","Runs a Rust `Future` on the current thread."]],"struct":[["JsAsyncRead",""],["JsFuture","A Rust `Future` backed by a JavaScript `Promise`."],["JsStream",""]]});
